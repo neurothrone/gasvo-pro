2f2f4ecf6b4fb69b1b456f59b28077ec974c89bc
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-04-25 21:52:28 +0200

    add gasvo theme colors

---
+++ lib/gasvo_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+const darkestBlue = Color(0xFF000814);
+const darkBlue = Color(0xFF001D3D);
+const lightBlue = Color(0xFF003566);
+const darkGold = Color(0xFFFFC300);
+const darkerGold = Color(0xFFB58B02);
+const lightGold = Color(0xFFFFD60A);
